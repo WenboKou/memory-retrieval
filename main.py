3736b75abb09545e657d6f16528a882231b46746
WenboKou <kouwenbocn@163.com>
2024-06-10 00:38:23 +0800

    完成text2embedding、add_texts_to_db和search_db_topk 用于将文本转换为向量并存储到向量数据库；用于基于余弦相似度召回topk近似的记忆。

---
+++ main.py
@@ -0,0 +1,82 @@
+import json
+import os
+
+import faiss
+import numpy as np
+import requests
+
+EMBEDDING_DIMENSION = 1536  # 向量维度
+
+
+def normalize_L2(data):
+    if isinstance(data, list):
+        data = np.array(data).reshape(1, -1)
+    return data / np.linalg.norm(data, axis=1, keepdims=True)
+
+
+def text2embedding(texts):
+    """
+
+    :param texts: ["文本0", ...]
+    :return:  [{"embedding": [vector], "text_index": 0}, ...]
+    """
+    # API的URL
+    url = "https://dashscope.aliyuncs.com/api/v1/services/embeddings/text-embedding/text-embedding"
+
+    # 请求体中的数据，通常是一个字典
+    json_data = {
+        "model": "text-embedding-v1",
+        "input": {
+            "texts": texts
+        },
+        "parameters": {
+            "text_type": "query"
+        }
+    }
+
+    # 自定义请求头
+    headers = {
+        "Authorization": os.getenv("DASHSCOPE_API_KEY"),
+        "Content-Type": "application/json"
+    }
+
+    # 发送POST请求，包含自定义请求头
+    response = requests.post(url, json=json_data, headers=headers)
+
+    # 检查请求是否成功
+    if not response.status_code == 200:
+        print("Failed with status code:", response.status_code)
+    return response.json()["output"]["embeddings"]
+
+
+def add_texts_to_db(texts, index, db):
+    """
+    将文本转换为向量，并存储到向量数据库和db中
+    :param texts: 文本列表
+    :param index: 向量数据库
+    :param db: ["0": text0, ...]
+    :return:
+    """
+    embeddings = text2embedding(texts)
+    for text, embedding in zip(texts, embeddings):
+        db[index.ntotal] = text
+        index.add(normalize_L2(embedding["embedding"]))
+    with open('db.json', 'w', encoding='utf-8') as file:
+        # 将Python对象转换为JSON格式并写入文件
+        json.dump(db, file, ensure_ascii=False)
+
+
+def search_db_topk(query, index, db, k=5):
+    embedding = text2embedding([query])[0]
+    distances, indices = index.search(normalize_L2(embedding["embedding"]), k)
+    print("距离：", distances)
+    print("索引：", indices)
+    print("索引数量：", index.ntotal)
+    for i in indices[0]:
+        print(f"{i}对应的文本：", db.get(i))
+
+
+index = faiss.IndexFlatIP(EMBEDDING_DIMENSION)  # 使用 Faiss 构建索引，这里使用 IndexFlatIP 来计算点积，相当于余弦相似度
+db = {}
+add_texts_to_db(["ddddd", "aaaaa"], index, db)
+search_db_topk("ddddddddd", index, db, k=5)
